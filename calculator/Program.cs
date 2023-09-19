08893dcf927d436814f6917b77332abd14c6a377
amjadkhansarwar <muhammad.amjad@iths.se>
2023-09-19 10:34:21 +0200

    start working on calculator

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue; 
+        string calculator = "Calculator";
+        int screenWidth = Console.WindowWidth;
+        int textWidth = calculator.Length;
+        int leftMargin = (screenWidth - textWidth) / 2;
+        string centeredText = new string(' ', leftMargin) + calculator;
+        Console.WriteLine(centeredText);
+
+        Console.WriteLine();
+        Console.WriteLine("Eller skriv avsluta");
+    }
+}
