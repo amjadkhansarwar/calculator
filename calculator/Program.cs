e358dac8a43d13f941e7c41a20e114289fcaa7b9
amjadkhansarwar <muhammad.amjad@iths.se>
2023-09-19 11:47:29 +0200

    working on read and write on file

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -11,8 +11,26 @@ class Program
         int leftMargin = (screenWidth - textWidth) / 2;
         string centeredText = new string(' ', leftMargin) + calculator;
         Console.WriteLine(centeredText);
+        var allLines = File.ReadAllLines("../../../Files/student.csv");
 
-        Console.WriteLine();
-        Console.WriteLine("Eller skriv avsluta");
+        List<string> ages = new List<string>();
+
+        foreach (var line in allLines)
+        {
+            string[] values = line.Split(',');
+            if(values.Length >= 2)
+            {
+                string age = values[1];
+                ages.Add(age);
+            }
+        }
+        foreach(string age in ages)
+        {
+            int ageValue;
+            ageValue = Convert.ToInt32(age);
+            if (ageValue % 2 == 0)
+                Console.WriteLine(ageValue);
+        }
+            Console.ReadLine();
     }
 }
